a642648018a12a0e706b075aefb850794530d75a
Christian <Christianmnaira@gmail.com>
2024-07-08 23:37:39 +0800

    feat: Implemented switching colors using useState hook

--- src/State.tsx
+++ src/State.tsx
@@ -3,32 +3,27 @@ import { useState } from "react";
 
 function State(){
 
-    const [car, setCar] = useState({
-        brand: 'Ford',
-        model: 'Mustang',
-        year: '1964',
-        color: 'red'
-    });
-
-    const updateColor = () => {
-
-        setCar(previousState => {
-            return {
-                ...previousState, color: "blue"
-            }
-        })
-
-    }
+    const [color, setColor] = useState("red");
 
     return(
         <>
 
-        <h1>My {car.brand}</h1>
-        <p>
-            It is a {car.color} {car.model} from {car.year}.
-        </p>
-        <button type="button" onClick={updateColor}>
+        <h1>My favorite color is {color}!</h1>
+
+        <button type="button" onClick={() => setColor("blue")}>
             Blue
+        </button>
+
+        <button type="button" onClick={() => setColor("red")}>
+            Red
+        </button>
+
+        <button type="button" onClick={() => setColor("pink")}>
+            Pink
+        </button>
+
+        <button type="button" onClick={() => setColor("green")}>
+            Green
         </button>
         
         </>
