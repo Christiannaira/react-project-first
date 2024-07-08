4cd1566d91c2a90d8756fc1c7d483fa6486fc76a
Christian <Christianmnaira@gmail.com>
2024-07-08 23:29:24 +0800

    feat: Implemented object using useState hook

--- src/State.tsx
+++ src/State.tsx
@@ -3,18 +3,20 @@ import { useState } from "react";
 
 function State(){
 
-    const [color, setColor] = useState('red');
-    const [brand, setBrand] = useState("Ford");
-    const [year, setYear] = useState("1964");
-    const [model, setModel] = useState("Mustang");
+    const [car, setCar] = useState({
+        brand: 'Ford',
+        model: 'Mustang',
+        year: '1964',
+        color: 'red'
+    });
 
     return(
         <>
 
-           <h1>My {brand}</h1>
-           <p>
-            It is a {color} {model} from {year}.
-           </p>
+        <h1>My {car.brand}</h1>
+        <p>
+            It is a {car.color} {car.model} from {car.year}.
+        </p>
         
         </>
     )
