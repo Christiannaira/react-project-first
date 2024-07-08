19c03793a99a5c6486c335bcde4049fdaf482b8d
Christian <Christianmnaira@gmail.com>
2024-07-08 23:45:20 +0800

    feat: Implemented useEffect & useState hooks

--- src/State.tsx
+++ src/State.tsx
@@ -4,24 +4,20 @@ import { useEffect, useState } from "react";
 function State(){
 
     const [count, setCount] = useState(0);
+    const [calculation, setCalculation] = useState(0);
 
     useEffect(() => {
 
-        setTimeout(
-            () => {
+           setCalculation(() => count * 2) 
 
-                setCount((count) => count + 1);
-
-            }, 1000
-        );
-
-    }, [])
+    }, [count])
 
 
     return(
         <>
-    <h1>I have rendered {count} times!</h1>
-        
+        <p>Count: {count}</p>
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <p>Calculation: {calculation}</p>
         </>
     )
 
