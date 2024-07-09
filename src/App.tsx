f5ed7615a35d22f2f954667a681d69c7d0e9e90d
Christian <Christianmnaira@gmail.com>
2024-07-09 10:48:04 +0800

    feat: Implemented && conditional operator

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,44 @@ import Conditional from "./Conditional"
 
 function App() {
 
+  const cars = ['Ford', 'BMW', 'Audi'];
+
   return (
     <>
-  
-   
-
 
     <div className="container mt-5 border">
       <Conditional isGoal={false}/>
     </div>
 
+    <div className="container bg-primary text-white mt-5 p-5">
+      
+      <Garage cars={cars} />
+
+    </div>
+
 
     </>
   )
+}
+
+function Garage(props){
+
+  const cars = props.cars;
+
+  return(
+    <>
+
+    <h1>Garage</h1>
+    {
+      cars.length > 0 && 
+      <h2>
+        You have {cars.length} cars in your garage.
+      </h2>
+    }
+    
+    </>
+  )
+
 }
 
 export default App
