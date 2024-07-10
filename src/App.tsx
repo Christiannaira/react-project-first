dc1b0dc71d6fa5891e4cd51689208154ae435d50
Christian <Christianmnaira@gmail.com>
2024-07-10 22:32:20 +0800

    fix: Removed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,19 @@
-import Student from "./Student";
+
 import Navbar from "./Navbar";
 import { useState } from "react";
 
 function App() {
 
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => (
-      {
-        ...values, [name]: value
-      }
-    ))
-
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(inputs);
-  }
-
-
-
 
   return (
     <>
-
-      <div className="container m-5 p-5">
-
-        <form action="" onSubmit={handleSubmit}>
-            <label htmlFor="">Enter your name:
-              <input 
-              type="text"
-              name="username" 
-              value={inputs.username || ""}
-              onChange={handleChange}
-              />
-            </label>
-
-            <label htmlFor="">Enter your age:
-              <input 
-              type="number" 
-              name="age"
-              value={inputs.age || ""}
-              onChange={handleChange}
-              />
-            </label>
-
-            <input type="submit" />
-        </form>
-
-    {inputs.username} <br />
-    {inputs.age}
-
-      </div>
-
+  hello world
     </>
   )
+
 }
+
+
 
 
 
