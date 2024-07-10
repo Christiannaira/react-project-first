90b0eca805d264fa336867bd39d4c1475ff41c53
Christian <Christianmnaira@gmail.com>
2024-07-10 13:44:31 +0800

    feat: Implemented map function in react to display lists

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@
 
 function App() {
 
-  const shoot = (a, b) => {
-
-    alert(b.type);
-
-  }
-
+  const cars = ['Ford', 'BMW', 'Audi'];
 
   return (
     <>
 
-  <button onClick={(event) => shoot("Goal!", event)}>
-    Take the shot!
-  </button>
-
+    <h1>Who lives in my garage?</h1>
+    <ul>
+      {cars.map((car) => <Lists brand={car} />)}
+    </ul>
+ 
     </>
   )
+}
+
+function Lists(props) {
+
+  return <li>I am a {props.brand}</li>
+
 }
 
 export default App
