07114c0c7a333ad566007369a1dcb8769edcd41b
Christian <Christianmnaira@gmail.com>
2024-07-10 14:06:43 +0800

    feat: map function & react props

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,27 @@ import Navbar from "./Navbar";
 
 function App() {
 
+  const students = [
+    {
+      firstname: 'John',
+      lastname: 'Smith',
+    },
+    {
+      firstname: 'Mark',
+      lastname: 'Zuckerberger',
+    },
+    {
+      firstname: 'Melon',
+      lastname: 'Musk'
+    }
+  ]
+
+  const studentList = students.map((item, id) => (
+
+    <Student key={id} firstname={item.firstname} lastname={item.lastname}/>
+    
+  ))
+
 
   return (
     <>
@@ -10,9 +31,7 @@ function App() {
       <div className="container m-5 p-5">
 
     <Navbar/>
-    <Student firstname={'Christian'} lastname={'Naira'} />
-    <Student firstname={'Vicente'} lastname={'Naira'} />
-    <Student firstname={'Jewer'} lastname={'Naira'} />
+    {studentList}
 
       </div>
 
