b3b69bb447d2ffc4131c590661cf5b9b8cbe8c7a
Christian <Christianmnaira@gmail.com>
2024-07-10 13:46:12 +0800

    feat: Added keys props

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@
 
 function App() {
 
-  const cars = ['Ford', 'BMW', 'Audi'];
+  const cars = [{
+    id: 1, brand: 'Ford'
+  },
+  {
+    id: 2, brand: 'BMW'
+  },
+  {
+    id: 3, brand: 'Audi'
+  }
+  ];
 
   return (
     <>
 
     <h1>Who lives in my garage?</h1>
     <ul>
-      {cars.map((car) => <Lists brand={car} />)}
+      {cars.map((car) => <Lists key={car.id} brand={car.brand} />)}
     </ul>
  
     </>
